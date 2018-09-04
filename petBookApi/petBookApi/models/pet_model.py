7caa52a9d6b22bb35fb334ac54b7e625acb87147
rsbabcock <rachael.s.babcock@gmail.com>
2018-09-04 13:31:50 -0500

    models coming together

--- petBookApi/petBookApi/models/pet_model.py
+++ petBookApi/petBookApi/models/pet_model.py
@@ -9,6 +9,25 @@ class Pet(models.Model):
     breed = models.ForeignKey('Breed', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='%m/%d')
+    nick_name = models.CharField(max_length=100)
+    birthday = models.CharField(max_length=100)
+    houdini = models.BooleanField(initial=True)
+    food_quirks = models.CharField(max_length=300)
+    crate_trained = models.BooleanField(initial=True)
+    crate_quirks = models.CharField(max_length=300)
+    walking_quirks = models.CharField(max_length=300)
+    potty_needs = models.CharField(max_length=300)
+    aggression_notes = models.CharField(max_length=300)
+    eating_times = models.CharField(max_length=300)
+    bed_time = models.CharField(max_length=300)
+    fav_toy = models.CharField(max_length=300)
+    deceased = models.BooleanField(initial=False)
+
+
+    def __str__(self):
+    return "{} {}".format(self.name, self.image)
+
+
 
     # def __str__(self):
     # return "{} {}".format(self.user.first_name, self.user.last_name)
--- petBookApi/petBookApi/models/pet_type_model.py
+++ petBookApi/petBookApi/models/pet_type_model.py
@@ -5,5 +5,6 @@ class PetType(models.Model):
     Model to represent pet types
     '''
     pet_type_name = models.CharField(max_length=200)
+    
     def __str__(self):
     return self.pet_type_name
