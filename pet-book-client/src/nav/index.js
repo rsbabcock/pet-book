675207331ccb89d6ece33036f8f12f2740043eab
rsbabcock <rachael.s.babcock@gmail.com>
2018-09-05 14:42:37 -0500

    ready to merge to master

--- pet-book-client/src/nav/index.js
+++ pet-book-client/src/nav/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import './nav.css';
 import logo from '../img/petBookLogo_white.png'
-// import {Navbar, Brand} from 'react-bootstrap'
 
 class Nav extends Component {
 
@@ -56,37 +55,3 @@ class Nav extends Component {
 }
 
 export default Nav
-
-// <Navbar inverse collapseOnSelect>
-//   <Navbar.Header>
-//     <Navbar.Brand>
-//       <img id="home" src={logo} alt="logo" /> 
-//       <h3>PetBook</h3>
-//     </Navbar.Brand>
-//     <Navbar.Toggle />
-//   </Navbar.Header>
-//   <Navbar.Collapse>
-//     <Nav>
-//      {isAuth &&
-//       <NavItem eventKey={1} href="#">
-//         Add Pet
-//       </NavItem>
-//       <NavItem eventKey={2} href="#">
-//         Follow
-//       </NavItem>
-//     </Nav>
-//     <Nav pullRight>
-//       <NavItem eventKey={1} href="#">
-//         Welcome, {this.props.user}
-//       </NavItem>
-//          }
-//       <NavItem eventKey={2} href="#">
-//         <button onClick={() => isAuth ? this.logOut() : this.displayLogin()}>
-//          Log {isAuth ? "out" : "in"} </button>
-//       </NavItem>
-//       <NavItem eventKey={3} href="#">
-//          <button onClick={() => this.displayRegister()}>Register</button>
-//       </NavItem>
-//     </Nav>
-//   </Navbar.Collapse>
-// </Navbar>;
